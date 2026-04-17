700f3da3459483b49d11b56b21390523f44585ba
New Name <new@email.com>
2026-04-17 11:54:11 +0800

    Add improved mobile responsive styles for order review page

--- includes/manager_sidebar.php
+++ includes/manager_sidebar.php
@@ -3,7 +3,14 @@
 $current_page = basename($_SERVER['PHP_SELF']);
 $user_name = $_SESSION['user_name'] ?? 'Manager';
 $user_initial = strtoupper(substr($user_name, 0, 1));
-require_once __DIR__ . '/shop_config.php';`r`n`r`n// Load config for BASE_PATH`r`nif (file_exists(__DIR__ . '/../config.php')) {`r`n    require_once __DIR__ . '/../config.php';`r`n}`r`n$base_path = defined('BASE_PATH') ? BASE_PATH : '/printflow';`r`n$logout_url = $base_path . '/logout';
+require_once __DIR__ . '/shop_config.php';
+
+// Load config for BASE_PATH
+if (file_exists(__DIR__ . '/../config.php')) {
+    require_once __DIR__ . '/../config.php';
+}
+$base_path = defined('BASE_PATH') ? BASE_PATH : '/printflow';
+$logout_url = $base_path . '/logout';
 
 // Get profile picture for sidebar
 $sidebar_profile_pic = '';
